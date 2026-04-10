3098e0ee6d0a705f233237e43766eabbcd512ce9
Gilberto Taccari <gilberto.taccari@gmail.com>
2026-04-10 18:23:00 +0200

    Configure status page
    Configure the status page that shops the historycal uptime of the
    website.

--- terraform/live/uptimerobot/main.tf
+++ terraform/live/uptimerobot/main.tf
@@ -13,3 +13,13 @@ resource "uptimerobot_monitor" "website" {
   url      = "https://gilbertotaccari.com"
   interval = 300
 }
+
+resource "uptimerobot_psp" "webisite" {
+  name = "gilbertotaccari.com Status"
+  monitor_ids = [
+    uptimerobot_monitor.website.id,
+  ]
+
+  # Prevent search engine indexing
+  no_index = true
+}
