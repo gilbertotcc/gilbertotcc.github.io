56e9ec1b9c265515383d4c9b80b434921e11c522
Gilberto Taccari <gilberto.taccari@gmail.com>
2026-05-01 11:31:57 +0200

    Add minor fixes

--- astro-site/src/config.ts
+++ astro-site/src/config.ts
@@ -4,10 +4,10 @@ export const SITE = {
   url: 'https://gilbertotaccari.com',
   lang: 'en',
   author: { name: 'Gilberto Taccari' },
-  googleAnalyticsId: 'G-XHFF332CDW',         // from _config.yml
-  cookiebotId: '01108c58-7b63-4ce1-ae70-3d7bdd95c7f3', // from _includes/cookiebot.html
+  googleAnalyticsId: 'G-XHFF332CDW',
+  cookiebotId: '01108c58-7b63-4ce1-ae70-3d7bdd95c7f3',
   social: {
     linkedin: 'https://www.linkedin.com/in/gilbertotaccari/',
     github: 'https://github.com/gilbertotcc',
   },
-} as const;
+};
